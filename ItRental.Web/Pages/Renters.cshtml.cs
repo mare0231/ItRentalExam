cadc1e712412d276e79832d30618032f6411504e
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-08 14:16:25 +0200

    Html form to create new renter is now created

--- ItRental.Web/Pages/Renters.cshtml.cs
+++ ItRental.Web/Pages/Renters.cshtml.cs
@@ -14,8 +14,27 @@ namespace ItRental.Web.Pages
         [BindProperty]
         public Renter Renter { get; set; }
         public List<Renter> Renters { get; set; } = new List<Renter>();
+        public List<Renter> RenterLevels { get; set; }
         public string Message { get; set; }
+
+        public RentersModel()
+        {
+            RenterRepository renterRepository = new RenterRepository();
+            Renters = renterRepository.GetRenters();
+            List<RenterLevel> RenterLevelList = new List<RenterLevel>() { RenterLevel.Starter, RenterLevel.Normal, RenterLevel.TopRenter };
+            RenterLevels = new List<Renter>();
+            foreach (RenterLevel renterLevel in RenterLevelList)
+            {
+                RenterLevels.Add(new Renter() { RenterLevel = renterLevel });
+            }
+        }
+
         public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
         {
             RenterRepository renterRepository = new RenterRepository();
             Renters = renterRepository.GetRenters();

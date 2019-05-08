d6de3d1713c9476d52696012eccbf07315ccb3e2
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-08 13:22:40 +0200

    Renter page created

---
+++ ItRental.Web/Pages/Renters.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ItRental.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ItRental.Web.Pages
+{
+    public class RentersModel : PageModel
+    {
+        [BindProperty]
+        public Renter Renter { get; set; }
+        public List<Renter> Renters { get; set; } = new List<Renter>();
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            
+        }
+    }
+}

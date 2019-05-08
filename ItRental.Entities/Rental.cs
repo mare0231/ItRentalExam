84b1671076ff36fb2f895fd57513b1dc2c36dfed
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-08 13:05:00 +0200

    Rental class created

---
+++ ItRental.Entities/Rental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItRental.Entities
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public DateTime RentalTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public Equipment Equipment { get; set; }
+        public int Units { get; set; }
+        public Renter Renter { get; set; }
+        public bool IsRentalOverdue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

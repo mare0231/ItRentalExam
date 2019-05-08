c1016c2d5f04c1ed9d9387f14dd4e595eafe1e7c
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-08 13:31:31 +0200

    Methods GetRenters() and HandleData() created in RenterRepository

--- ItRental.Dal/RenterRepository.cs
+++ ItRental.Dal/RenterRepository.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using ItRental.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace ItRental.Dal
 {
     class RenterRepository : BaseRepository
     {
+        public List<Renter> GetRenters()
+        {
+            string sql = "SELECT * FROM Renters";
+            return HandleData(ExecuteQuery(sql));
+        }
+        private List<Renter> HandleData(DataTable dataTable)
+        {
+            List<Renter> renters = new List<Renter>();
+            if (dataTable is null)
+                return renters;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Renter renter = new Renter()
+                {
+                    Id = (int)row["RenterId"],
+                    Name = (string)row["Name"],
+                    RenterLevel = (RenterLevel)row["RenterLevel"]
+                };
+                renters.Add(renter);
+            }
+            return renters;
+        }
     }
 }

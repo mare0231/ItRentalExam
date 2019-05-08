1f12ee293dac07b3ec3402f3253c2732d859eb04
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-08 13:00:13 +0200

    Renter class created

---
+++ ItRental.Entities/Renter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItRental.Entities
+{
+    class Renter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public RenterLevel RenterLevel { get; set; }
+        public List<Rental> Rentals { get; set; }
+        public int NumberOfRentals { get; }
+        public Rental NextRentalDue()
+        {
+            throw new NotImplementedException();
+        }
+        public bool GotOverdueRental()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

2c4b092896f5e20ff208c982ea640740b875bf81
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-09 09:47:09 +0200

    Opening and OpeningHours class created for opening API

---
+++ ItRental.Entities/OpeningAPI/Opening.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItRental.Entities.OpeningAPI
+{
+    public class Opening
+    {
+        public List<OpeningHours> OpeningHours { get; set; }
+    }
+}
---
+++ ItRental.Entities/OpeningAPI/OpeningHours.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItRental.Entities.OpeningAPI
+{
+    public class OpeningHours
+    {
+        public string Day { get; set; }
+        public DateTime OpeningHour { get; set; }
+        public DateTime ClosingHour { get; set; }
+    }
+}

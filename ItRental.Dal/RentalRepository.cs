66a3b209559fe9f523eb8bd9101e9890afcd5c3b
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-09 10:48:07 +0200

    Method to handle data in RentalRepository created

--- ItRental.Dal/RentalRepository.cs
+++ ItRental.Dal/RentalRepository.cs
@@ -8,5 +8,28 @@ namespace ItRental.Dal
 {
     public class RentalRepository : BaseRepository
     {
+        private List<Rental> HandleData(DataTable dataTable)
+        {
+            List<Rental> rentals = new List<Rental>();
+            if (dataTable is null)
+                return rentals;
+
+            RenterRepository renterRepository = new RenterRepository();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Rental rental = new Rental()
+                {
+                    Id = (int)row["RentalId"],
+                    RentalTime = (DateTime)row["RentalTime"],
+                    ReturnTime = (DateTime)row["ReturnTime"],
+                    Equipment = (Equipment)row["Equipment"],
+                    Units = (int)row["Units"],
+                    Renter = renterRepository.GetRenter((int)row["RenterId"])
+                };
+                rentals.Add(rental);
+            }
+            return rentals;
+        }
     }
 }

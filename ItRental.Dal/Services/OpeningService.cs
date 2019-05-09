9b1a495f2c503f2e7d8528a48f92e4b48c97b0de
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-05-09 09:49:48 +0200

    Method to get opening hours created in the OpeningService class

--- ItRental.Dal/Services/OpeningService.cs
+++ ItRental.Dal/Services/OpeningService.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using ItRental.Entities.OpeningAPI;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace ItRental.Dal.Services
 {
     public class OpeningService
     {
+        public Opening GetOpeningHours()
+        {
+            string json;
+            using (WebClient webClient = new WebClient())
+            {
+                json = webClient.DownloadString($"http://api.aspitcloud.dk/openinghours");
+            }
+            return JsonConvert.DeserializeObject<Opening>(json);
+        }
     }
 }
